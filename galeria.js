11974ec73c1669e605d4da267500513740d5e049
FernandoFiore07 <fernando.fiore@estudiantes.utec.edu.uy>
2025-07-21 20:55:29 -0300

    Se creo el archivo de js y se paso el script de modo oscuro y se creo el boton de like, se le agregaron los estilos y se linkeo el js al html
    Co-authored-by: GosGuru <GosGuru@users.noreply.github.com>
    Co-authored-by: JesicaBignoli <JesicaBignoli@users.noreply.github.com>
    Co-authored-by: Bernardo Collins <hungaro23@users.noreply.github.com>

---
+++ galeria.js
@@ -0,0 +1,51 @@
+//Se define la funcion llamada agregarBotones
+// Dentro de la funcio se seleccionan todos los elementos con la clase .album
+function agregarBotonesLike() {
+  const albums = document.querySelectorAll(".album");
+
+  //Recorre cada uno de los a
+  albums.forEach((album) => {
+    //Crear el boton en cada tarjeta le agrega una clase para poder darle estilo
+    const likeBtn = document.createElement("button");
+    likeBtn.classList.add("botonMeGusta");
+
+    //Crear el icono dentro del boton
+    const icono = document.createElement("i");
+    icono.classList.add("bx", "bx-plus-circle");
+
+    //Evento de clic para alternar la clase "liked"
+    likeBtn.onclick = function () {
+      icono.classList.toggle("liked");
+      const isLiked = icono.classList.contains("bxs-check-circle");
+
+      if (isLiked) {
+        icono.classList.remove("bxs-check-circle");
+        icono.classList.add("bx-plus-circle");
+      } else {
+        icono.classList.remove("bx-plus-circle");
+        icono.classList.add("bxs-check-circle");
+      }
+
+      album.classList.toggle("succesfull");
+    };
+
+    //Armar el boton
+    likeBtn.appendChild(icono);
+    album.appendChild(likeBtn);
+  });
+}
+
+const toggleBtn = document.getElementById("modo-toggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("modo-claro");
+  if (body.classList.contains("modo-claro")) {
+    toggleBtn.textContent = "🌚 Modo Oscuro";
+  } else {
+    toggleBtn.textContent = "🌙 Modo Claro";
+  }
+});
+
+//Ejecutar cuando cargue la pagina
+window.onload = agregarBotonesLike;
